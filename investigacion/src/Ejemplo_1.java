07cbc35a511f84a1d0f6c61e7434951a571377ae
Sebastian soto <juancarloshj25@gmail.com>
2026-07-26 18:08:40 -0500

    Ejemplo 1 echo

--- investigacion/src/Ejemplo_1.java
+++ investigacion/src/Ejemplo_1.java
@@ -1,2 +1,13 @@
 public class Ejemplo_1 {
+    public static void main(String[] args) {
+        /* Realizar un programa que imprima los números del 1 al 10  */
+
+        int i = 1;
+
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 10);
+
+    }
 }
