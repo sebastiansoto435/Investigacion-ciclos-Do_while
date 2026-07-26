392d069bbb04bb1a052823c0c275dcee017da088
Sebastian soto <juancarloshj25@gmail.com>
2026-07-26 18:17:01 -0500

    Ejemplo 3 echo

---
+++ investigacion/src/Ejemplo_3.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class Ejemplo_3 {
+    public static void main(String[] args) {
+
+        int numero;
+        int contador = 0;
+
+        do {
+
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número (-1 para salir): "));
+
+            if (numero > 0) {
+                contador++;
+            }
+
+        } while (numero != -1);
+
+        System.out.println("Cantidad de números positivos: " + contador);
+
+    }
+}
